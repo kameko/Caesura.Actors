cd3f10b5ef225061f10c6236f322b59163adc6ad
Kameko Kirishiki <kameko.k@outlook.com>
2019-12-02 15:02:42 -0500

    Init

---
+++ Caesura.Actors/Actor.cs
@@ -0,0 +1,13 @@
+
+namespace Caesura.Actors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    
+    public class Actor
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+
+A simple actor system inspired by Akka.NET, designed to be simpler, easier to use, more predictable, less overly-engineered, and designed with plugins (AssemblyLoadContext) in mind.
